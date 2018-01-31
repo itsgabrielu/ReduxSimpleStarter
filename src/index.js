07bd91354f1407ecd2312912304d560f248d1993
Gabriel Lu <gabriellu@Mr.Rayler>
2018-01-31 17:40:40 +0800

    Completed react tutorial for youtube browser

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
@@ -9,29 +10,36 @@ import VideoDetail from './components/video_detail.js'
 
 const API_KEY = 'AIzaSyBC0mdClPhZwm7QZRmvXTCFEUCEJ4VOjDw'
 
-
 class App extends Component {
   constructor(props){
     super(props)
 
     this.state={
-      videos: []
+      videos: [],
+      selectedVideo: null
     }
 
-    YTSearch({key: API_KEY, term: 'Javascript'},
+    this.videoSearch('surfboards')
+  }
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term},
       videos => {
         this.setState({
-          videos
+          videos: videos,
+          selectedVideo: videos[0]
         })
       }
     )
   }
-  render()  {
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
     return (
         <div>
-        <Searchbar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <Searchbar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
         </div>
       )
   }
